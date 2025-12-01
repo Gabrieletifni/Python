101e52a36847df8d1ddca597d4e6d0e84ff4ebdc
Gabriele Tifni <tifnigabriele@gmail.com>
2025-12-01 19:48:52 +0100

    Test

---
+++ test.py
@@ -0,0 +1,7 @@
+#Primo codice Github, calcolo la media tra due valori dati in ingresso
+A=int(input("Inserire il primo valore: "))
+B=int(input("Inserire il secondo valore: "))
+somma=A+B
+valori=2
+media=somma/valori
+print(f"La media vale {media}")
