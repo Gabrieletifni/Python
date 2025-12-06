e3603c1a45a6b4459758fc172cb4e3927b21a250
Gabriele Tifni <tifnigabriele@gmail.com>
2025-12-06 10:38:49 +0100

    03.1.2

---
+++ LAB/LAB3/03.1.2.py
@@ -0,0 +1,15 @@
+"""  
+03.1.2 Identikit della stringa. Scrivere un programma che legga una stringa e visualizzi i messaggi 
+appropriati, dopo aver verificato se:  
+I. contiene soltanto lettere; 
+II. contiene soltanto lettere maiuscole;  
+III. contiene soltanto lettere minuscole; 
+IV. contiene soltanto cifre numeriche decimali; 
+V. contiene soltanto lettere e cifre;  
+VI. inizia con una lettera maiuscola; 
+VII. termina con un punto. 
+[P3.17] 
+
+
+"""
+parola=input("Inserire una parola: ")
