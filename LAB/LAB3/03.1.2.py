e6e5c08eda6c5118e6abfb4bd02964f414380af9
Gabriele Tifni <tifnigabriele@gmail.com>
2025-12-06 14:48:52 +0100

    03.1.2

--- LAB/LAB3/03.1.2.py
+++ LAB/LAB3/03.1.2.py
@@ -12,4 +12,23 @@ VII. termina con un punto.
 
 
 """
+from string import *
 parola=input("Inserire una parola: ")
+if parola.isalpha():
+    print(f"La parola {parola} contiene solo lettere")
+else:
+    print(f"La parola {parola} non contiene solo lettere")
+if parola.isalpha() and parola.isupper():
+    print(f"La parola {parola} contiene solo lettere maiuscole")
+if parola.isalpha() and parola.islower():
+    print(f"La parola {parola} contiene solo lettere minuscole")
+if parola.isdigit():
+    print(f"La parola {parola} contiene solo numeri")
+if parola.isalnum():
+    print(f"La parola {parola} contiene cifre e lettere")
+prima_lettera=parola[0]
+if  prima_lettera.isupper():
+    print(f"La parola {parola} inizia con una lettera maiuscola")
+
+if parola.endswith("."):
+    print(f"La parola {parola} termina con un punto")
